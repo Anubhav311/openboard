b559330596ea4e80db84cc8126d5a360acd1ac78
Anubhav Bhambri <anubhavbhambri1@gmail.com>
2023-05-09 23:37:58 +0530

    added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import './styles/global.css'
+import './styles/home.css'
 // import './styles.css'
 // import IMAGE from './react.png'
 // import LOGO from './logo.svg'
@@ -91,52 +93,14 @@ membersList = membersList.sort((a: any, b: any) =>
 
 export const App = () => {
   const [members] = useState(membersList)
-  // const [contri, setContri] = useRecoilState(todoState)
-  // useEffect(async () => {
-  //   const url = 'https://api.github.com/users/Anubhav311/events'
-  //   const resp = await fetch(url)
-  //   const result = await resp.json()
-  //   setContri(result)
-  // }, [])
-
-  const personName = {
-    first: 'Bruce',
-    last: 'Wayne',
-  }
-
-  const nameList = [
-    {
-      first: 'Bruce',
-      last: 'Wayne',
-    },
-    {
-      first: 'Clark',
-      last: 'Kent',
-    },
-    {
-      first: 'Princess',
-      last: 'Diana',
-    },
-  ]
 
   return (
     <>
-      <Greet name="Anubhav" messageCount={10} isLoggedIn={false} />
-      <Person name={personName} />
-      <PersonList names={nameList} />
-      {/* {contri.map((todo) => (
-        <div key={todo.id}>
-          <div>{todo.id}</div>
-          <div>{todo.type}</div>
-          <div>{todo.created_at}</div>
-          <div>{todo.public}</div>
-        </div>
-      ))} */}
       <div className="container">
         <main className="main">
           <div>
             <div>
-              <h3>Epvi Engineering Leader Board</h3>
+              <h3>DSA Mastery Leader Board</h3>
             </div>
             <div>
               <table>
@@ -188,61 +152,5 @@ function TableBody(props: MembersListProps) {
         </tr>
       ))}
     </tbody>
-  )
-}
-
-// string, number and boolean types
-type GreetProps = {
-  name: string
-  messageCount: number
-  isLoggedIn: boolean
-}
-
-function Greet(props: GreetProps) {
-  return (
-    <h1>
-      {props.isLoggedIn
-        ? `Hello {props.name}. You have {props.messageCount} unread messages.`
-        : 'Welcome Guest'}
-    </h1>
-  )
-}
-
-// object props
-type PersonProps = {
-  name: {
-    first: string
-    last: string
-  }
-}
-
-function Person(props: PersonProps) {
-  return (
-    <div>
-      {props.name.first} {props.name.last}
-    </div>
-  )
-}
-
-// list props
-type PersonListProps = {
-  names: {
-    first: string
-    last: string
-  }[]
-}
-
-function PersonList(props: PersonListProps) {
-  return (
-    <div>
-      {props.names.map((name, i) => (
-        <h2 key={i}>
-          {name.first} {name.last}
-        </h2>
-      ))}
-      <h2>Bruce Wayne</h2>
-      <h2>Clark Kent</h2>
-      <h2>Princes Diana</h2>
-    </div>
   )
 }
