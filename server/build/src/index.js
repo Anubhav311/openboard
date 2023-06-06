16827b064c840213944c60d5ae6b0001a77e4773
Anubhav Bhambri <anubhavbhambri1@gmail.com>
2023-06-07 03:20:52 +0530

    removed port number

--- server/build/src/index.js
+++ server/build/src/index.js
@@ -39,7 +39,7 @@ const app = (0, express_1.default)();
 app.get("/", (req, res) => {
     res.send({ response: "Hello World" });
 });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Listening on port`, PORT);
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -43,7 +43,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send({ response: "Hello World" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Listening on port`, PORT);
 });
