34096143123d62a7201459f9523fd5eec55dcd3d
Anubhav Bhambri <anubhavbhambri1@gmail.com>
2023-06-07 14:13:41 +0530

    updated build

--- server/build/src/index.js
+++ server/build/src/index.js
@@ -6,59 +6,46 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const body_parser_1 = __importDefault(require("body-parser"));
-const node_fetch_1 = __importDefault(require("node-fetch"));
+const axios_1 = __importDefault(require("axios"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-// const app = express();
+app.use((0, cors_1.default)());
 app.use(body_parser_1.default.json());
-// app.use(
-//   cookieSession({
-//     maxAge: 30 * 24 * 60 * 60 * 1000,
-//     keys: [keys.cookieKey],
-//   })
-// );
-// app.use(passport.initialize());
-// app.use(passport.session());
-// if (["production"].includes(process.env.NODE_ENV)) {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve("client", "build", "index.html"));
-//   });
-// }
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 app.get("/getAccessToken", async (req, res) => {
-    console.log(req.query.code);
     const params = "?client_id=" +
         CLIENT_ID +
         "&client_secret=" +
         CLIENT_SECRET +
         "&code=" +
         req.query.code;
-    const response = await (0, node_fetch_1.default)("https://github.com/login/oauth/access_token" + params, {
+    const options = {
         method: "POST",
         headers: {
             Accept: "application/json",
         },
-    });
-    const data = response.json();
-    console.log(data);
-    res.json(data);
+        data: { data: "data" },
+        url: "https://github.com/login/oauth/access_token" + params,
+    };
+    const data = await (0, axios_1.default)(options);
+    const payload = { ...data.data };
+    res.json(payload);
 });
 // getUserData
 app.get("/getUserData", async (req, res) => {
-    req.get("Authorization");
-    const response = await (0, node_fetch_1.default)("https://api.github.com/user", {
+    const options = {
         method: "GET",
         headers: {
-            Authorization: "Bearer" + req.get("Authorization"), // Bearer Access Token
+            Authorization: req.get("Authorization"), // Bearer Access Token
         },
-    });
-    const data = response.json();
-    console.log(data);
-    res.json(data);
+        url: "https://api.github.com/user",
+    };
+    const data = await (0, axios_1.default)(options);
+    const payload = { ...data.data };
+    res.json(payload);
 });
-// app.get("/", (req, res) => {
 app.get("/", (req, res) => {
     res.send({ response: "Hello World", test: process.env.TEST_VARIABLE });
 });
